21f26885abba4b8f46fca60a194cb9f358eb4075
Semiu Mukaila <semiteagain@gmail.com>
2023-09-03 21:04:44 +0100

    adding leading container

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,6 +19,13 @@ class HomePage extends StatelessWidget {
         backgroundColor: HomeConstants.appBarBackground,
         elevation: 0.0,
         centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
       ),
     );
   }
